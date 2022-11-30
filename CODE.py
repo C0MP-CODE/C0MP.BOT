0fd2e21dfd0353bbae7dddf4eecd962affec4d0d
Samuel Sampaio <82287232+C0MPL3Xscs@users.noreply.github.com>
2022-11-30 14:29:41 +0000

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -15,18 +15,25 @@ async def on_ready():
             choices.append(f"#{channel.name} ({channel.id})")
 
     #Show choices and questions, get user input for questions
-    questions = [inquirer.List('cnl', message="What channel would you like to send the message to?", choices=choices, carousel=True), inquirer.Text('Title', message="What would you like the Title to be?"), inquirer.Text('Description', message="What would you like the description to be?"), inquirer.Text('url', message="What is the image url?"),]
-    
-    answers = inquirer.prompt(questions)
-    cnlid = int(answers['cnl'][10:-1])
-    cnl = guild.get_channel(cnlid)
+    questions = [inquirer.List('cnl', message="What channel would you like to send the message to?", choices=choices, carousel=True), inquirer.Text('Title', message="What would you like the Title to be?"), inquirer.Text('Description', message="What would you like the description to be?"), inquirer.Text('url', message="What is the image url?"), inquirer.Text('price', message="What is the product price?"),]
 
+    #Get the selected channel id and save the channel into "cnl"
+    answers = inquirer.prompt(questions)
+    channel = answers['cnl'].replace(")", "").split("(")[1]
+    cnlid = int(channel)
+    cnl = guild.get_channel(cnlid)
+    
     #Embed and send message
     embed = nextcord.Embed(title=answers['Title'], description=answers['Description'], color=0xAAFF00)
     if answers['url'] != "" and answers['url'].startswith('ht'): 
         embed.set_image(url = answers['url'])
+    if answers['price'] != "":
+        message = client.get_channel(1046435381148668013).mention
+        embed.add_field(name="PRICE:", value=answers['price'], inline=False)
+        embed.add_field(name="Open a ticket at:", value=message, inline=False)
     await cnl.send(embed=embed)
 
+    #If message sent print on console "Message Sent"
     print("Message Sent")
 
 client.run(token)
